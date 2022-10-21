30015285c325bb167ea2156b964bdf9e85dfb759
akoredewale <akoredewale07@gmail.com>
2022-10-21 10:23:20 -0700

    message

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: number of times the character should be printed
+ * Return: Always 0
+ **/
+
+void print_line(int n)
+{
+	int len;
+
+	if (n > 0)
+	{
+		for (len = 0; len < n; len++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
