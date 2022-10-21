1f509aee6e91385411b5e3758acaff3eec6a7fdf
akoredewale <akoredewale07@gmail.com>
2022-10-21 09:27:21 -0700

    message

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - checks if the character is multiplied
+ * @a: the first integer
+ * @b: the second integer
+ * Return: The multiplication of a and b
+ **/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
