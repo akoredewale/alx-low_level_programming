06395f1aa62570f6004bfefeeccba2d9bb4b6bf9
akoredewale <akoredewale07@gmail.com>
2022-10-21 11:29:16 -0700

    message

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,17 +7,17 @@
 
 void print_diagonal(int n)
 {
-	int x, y;
+	int i, j;
 
-	if (n <= 0)
-		_putchar('\n');
-	for (x = 0; x < n; x++)
+	for (i = 0; i < n; i++)
 	{
-		for (y = 0; y < n; y++)
+		for (j = 0; j < i; j++)
+		{
 			_putchar(' ');
-
-		_putchar('\\');
-
-		_putchar('\n');
+		}
+		_putchar(92);
+		if (i < (n - 1))
+			_putchar('\n');
 	}
+	_putchar('\n');
 }
